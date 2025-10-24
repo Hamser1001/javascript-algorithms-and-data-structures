4b9f8d1ddb5646f7fc697acb174abf54df6b8816
Hamser1001 <upscale.mind50@gmail.com>
2025-10-24 16:32:13 +0100

    update

--- pyramid-generator/main.js
+++ pyramid-generator/main.js
@@ -1,6 +1,8 @@
-console.log("Let's build a pyramid using JavaScript basics:");
-console.log("    #    ");
-console.log("   ###   ");
-console.log("  #####  ");
-console.log(" ####### ");
-console.log("#########");
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
---
+++ pyramid-generator/main.py
@@ -0,0 +1,8 @@
+character = "Hello"
+count = 8
+rows = ["Naomi", "Quincy", "CamperChan"]
+pushed = rows.append("freeCodeCamp")
+print(pushed)
+popped = rows.pop()
+print(popped)
+print(rows)
