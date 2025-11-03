f4ed93bd971ff2129972e9f6c7e43e9bd82bf78d
Hamser1001 <upscale.mind50@gmail.com>
2025-11-03 18:23:47 +0100

    update

--- music-player/script.js
+++ music-player/script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -133,7 +134,11 @@ const playPreviousSong = () => {
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
-  
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
@@ -170,6 +175,8 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
