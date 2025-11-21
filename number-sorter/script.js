137accaa0c0e93952d53bafb55e65e60c12029c3
Hamser1001 <upscale.mind50@gmail.com>
2025-11-21 13:24:00 +0100

    update

--- number-sorter/script.js
+++ number-sorter/script.js
@@ -38,9 +38,19 @@ const selectionSort = (array) => {
         let minIndex = i;
 
         for (let j = i + 1; j < array.length; j++) {
-
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
         }
+
+        const temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
     }
+
+    return array;
 }
+
+
 
 sortButton.addEventListener("click", sortInputArray);
