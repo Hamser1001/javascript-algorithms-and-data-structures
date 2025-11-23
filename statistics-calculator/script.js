ff934247c2d727d73f275469a59a7b28f77706b3
Hamser1001 <upscale.mind50@gmail.com>
2025-11-23 17:13:18 +0100

    update

--- statistics-calculator/script.js
+++ statistics-calculator/script.js
@@ -1,21 +1,25 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-
-
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+    const sorted = array.toSorted((a, b) => a - b);
+    const median =
+        sorted.length % 2 === 0
+            ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+            : sorted[Math.floor(sorted.length / 2)];
+    return median;
 }
 
-const calculate = () => {
-  const value = document.querySelector("#numbers").value;
-  const array = value.split(/,\s*/g);
-  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
-  const mean = getMean(numbers);
 
-  document.querySelector("#mean").textContent = mean;
+
+
+const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+
+    const mean = getMean(numbers);
+    const median = getMedian(numbers);
+
+    document.querySelector("#mean").textContent = mean;
+    document.querySelector("#median").textContent = median;
 }
