d1bb42b1410779667b5e2f073561ebb297f76236
Hamser1001 <upscale.mind50@gmail.com>
2025-11-23 19:21:45 +0100

    update

--- statistics-calculator/script.js
+++ statistics-calculator/script.js
@@ -29,7 +29,12 @@ const getRange = (array) => {
     return Math.max(...array) - Math.min(...array);
 }
 
-
+const getVariance = (array) => {
+    const mean = getMean(array);
+    const differences = array.map(
+        el => el - mean
+    );
+}
 
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
