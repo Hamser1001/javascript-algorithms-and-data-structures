6df2333b8507d45d74ec61e9997fb514a0b00be2
Hamser1001 <upscale.mind50@gmail.com>
2025-11-27 19:15:24 +0100

    update

--- spreadsheet/script.js
+++ spreadsheet/script.js
@@ -6,17 +6,25 @@ const median = nums => {
     const sorted = nums.slice().sort((a, b) => a - b);
     const length = sorted.length;
     const middle = length / 2 - 1;
+    return isEven(length)
+        ? average([sorted[middle], sorted[middle + 1]])
+        : sorted[Math.ceil(middle)];
+}
 
-    if (isEven(length)) {
-        return average([sorted[middle], sorted[middle + 1]]);
-    } else {
-        const midIndex = Math.floor(length / 2);
-        return sorted[midIndex];
-    }
-};
+const spreadsheetFunctions = {
+    sum,
+    average,
+    median
+}
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
+
+const evalFormula = (x, cells) => {
+    const idToText = id => cells.find(cell => cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+
+}
 
 window.onload = () => {
     const container = document.getElementById("container");
@@ -35,7 +43,16 @@ window.onload = () => {
             input.type = "text";
             input.id = letter + number;
             input.ariaLabel = letter + number;
+            input.onchange = update;
             container.appendChild(input);
         })
     })
+}
+
+const update = event => {
+    const element = event.target;
+    const value = element.value.replace(/\s/g, "");
+    if (!value.includes(element.id) && value.startsWith('=')) {
+
+    }
 }
