456d9225f6ba4b6f249a5c7344c8e3e260f970e4
Hamser1001 <upscale.mind50@gmail.com>
2025-11-29 21:19:39 +0100

    update

--- certification-projects/telephone-number-validator/script.js
+++ certification-projects/telephone-number-validator/script.js
@@ -7,3 +7,21 @@
 555-555-5555
 (555)555-5555
 */
+
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+
+checkBtn.addEventListener("click", () => {
+    if (userInput.value === "") {
+        alert("Please provide a phone number");
+    }
+    console.log(userInput.value);
+    result.innerText = userInput.value;
+})
+
+clearBtn.addEventListener("click", () => {
+    userInput.value = "";
+    result.innerText = "";
+})
