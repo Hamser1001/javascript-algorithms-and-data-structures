08a1b665b43c9357223777f6e71b04bf22d4d48a
Hamser1001 <upscale.mind50@gmail.com>
2025-12-19 15:01:41 +0100

    update

--- certification-projects/cash-register/script.js
+++ certification-projects/cash-register/script.js
@@ -12,9 +12,9 @@ One Hundred Dollars  $100.00 (ONE HUNDRED)
 */
 
 // DOM element references used for the cash register operation
-const changeDue = document.getElementById("change-due");
-const cash = document.getElementById("cash");
-const purchaseBtn = document.getElementById("purchase-btn");
+const changeDue = document.getElementById("change-due"); // Amount due for refund
+const cash = document.getElementById("cash"); // Amount paid
+const purchaseBtn = document.getElementById("purchase-btn"); // Purchase button
 const priceScreen = document.getElementById("price-screen");
 
 let price = 1.87;
@@ -30,8 +30,26 @@ let cid = [
     ['ONE HUNDRED', 100]
 ];
 
+let obj = {}
+
+
+const calculateChange = () => {
+    cid.forEach((element) => {
+        obj[element[0]] = element[1];
+        console.log(element[0]);
+    })
+
+    for (const key in obj) {
+        console.log(`this is a key: ${key}`);
+
+    }
+    console.log(`obj is : ${obj}`)
+}
+
 
 purchaseBtn.addEventListener("click", () => {
+    calculateChange();
+    // to know the button works
     console.log("Clicked");
-    changeDue.innerHTML = `<h1>Clicked</h1>`;
+    changeDue.innerHTML += `<p>Clicked</p>`;
 })
