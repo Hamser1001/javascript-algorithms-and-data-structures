ae7e2a1ce1b72e1617ee737df3bae4e2657c5186
Hamser1001 <upscale.mind50@gmail.com>
2025-12-19 13:09:11 +0100

    testing event of purchase Button

--- certification-projects/cash-register/script.js
+++ certification-projects/cash-register/script.js
@@ -1,3 +1,22 @@
+/*
+Currency Unit        Amount
+Penny                $0.01 (PENNY)
+Nickel               $0.05 (NICKEL)
+Dime                 $0.10 (DIME)
+Quarter              $0.25 (QUARTER)
+Dollar               $1.00 (ONE)
+Five Dollars         $5.00 (FIVE)
+Ten Dollars          $10.00 (TEN)
+Twenty Dollars       $20.00 (TWENTY)
+One Hundred Dollars  $100.00 (ONE HUNDRED)
+*/
+
+// DOM element references used for the cash register operation
+const changeDue = document.getElementById("change-due");
+const cash = document.getElementById("cash");
+const purchaseBtn = document.getElementById("purchase-btn");
+const priceScreen = document.getElementById("price-screen");
+
 let price = 1.87;
 let cid = [
     ['PENNY', 1.01],
@@ -10,3 +29,9 @@ let cid = [
     ['TWENTY', 60],
     ['ONE HUNDRED', 100]
 ];
+
+
+purchaseBtn.addEventListener("click", () => {
+    console.log("Clicked");
+    changeDue.innerHTML = `<h1>Clicked</h1>`;
+})
