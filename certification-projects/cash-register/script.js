84bed744305bb224be419ac9486139824bbe4173
Hamser1001 <upscale.mind50@gmail.com>
2025-12-22 17:50:07 +0100

    update

--- certification-projects/cash-register/script.js
+++ certification-projects/cash-register/script.js
@@ -4,7 +4,7 @@ const cash = document.getElementById("cash"); // Amount paid
 const purchaseBtn = document.getElementById("purchase-btn"); // Purchase button
 const priceScreen = document.getElementById("price-screen");
 
-let price = 1.87;
+let price = 3.26;
 let cid = [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
@@ -24,21 +24,21 @@ class CashRegister {
         this.price = price;
         this.currency = {
             // unit key will be added in next for loop
-            PENNY: { label: "Penny", value: 0.01 },
-            NICKEL: { label: "Nickel", value: 0.05 },
-            DIME: { label: "Dime", value: 0.10 },
-            QUARTER: { label: "Quarter", value: 0.25 },
-            ONE: { label: "One", value: 1.00 },
-            FIVE: { label: "Five", value: 5.00 },
-            TEN: { label: "Ten", value: 10.00 },
-            TWENTY: { label: "Twenty", value: 20.00 },
-            "ONE HUNDRED": { label: "One Hundred", value: 100.00 }
+            PENNY: { label: "Pennies", value: 0.01 },
+            NICKEL: { label: "Nickels", value: 0.05 },
+            DIME: { label: "Dimes", value: 0.10 },
+            QUARTER: { label: "Quarters", value: 0.25 },
+            ONE: { label: "Ones", value: 1.00 },
+            FIVE: { label: "Fives", value: 5.00 },
+            TEN: { label: "Tens", value: 10.00 },
+            TWENTY: { label: "Twenties", value: 20.00 },
+            "ONE HUNDRED": { label: "Hundreds", value: 100.00 }
         };
     }
 
     // Display Change in drawer
     displayCid() {
-        // Create a p element to display the Total
+        // Create a p element to display the TotalchangeDue
         const total = document.createElement('p');
         priceScreen.appendChild(total);
         // Create a unorderd list element to display the 
@@ -53,7 +53,7 @@ class CashRegister {
         }
     }
 
-    calculate(amount) {
+    calculateChangeDue(amount) {
         amount = Number(amount);
         let changeDue = {};
 
@@ -70,11 +70,25 @@ class CashRegister {
                 } else {
                     changeDue[cid[i][0]] = { label: this.currency[cid[i][0]].label, value: this.currency[cid[i][0]].value, unit: 1 };
                 }
-                amount -= this.currency[cid[i][0]].value;
+                amount -= Number(this.currency[cid[i][0]].value.toFixed(2));
                 this.currency[cid[i][0]].unit--;
             }
         }
+
+        console.log(this.cid);
         console.log(changeDue);
+        displayChangeDue.innerHTML = "";
+        for (let i = 0; i < cid.length; i++) {
+            if (changeDue[cid[i][0]]) {
+                // console.log(changeDue[cid[i][0]]);
+                cid[i][1] = Number((cid[i][1] - changeDue[cid[i][0]].value * changeDue[cid[i][0]].unit).toFixed(2));
+                console.log(`${cid[i][1]} - ${changeDue[cid[i][0]].value} * ${changeDue[cid[i][0]].unit} = ${cid[i][1] - changeDue[cid[i][0]].value * changeDue[cid[i][0]].unit}`);
+                displayChangeDue.innerHTML += `<p class="clicked">${cid[i][0]}: $${changeDue[cid[i][0]].value * changeDue[cid[i][0]].unit}</p>`;
+            }
+        }
+        console.log(this.cid);
+        priceScreen.innerHTML = "";
+        this.displayCid();
     }
 }
 
@@ -99,10 +113,9 @@ cashClass.displayCid();
 purchaseBtn.addEventListener("click", () => {
     // Calculate the change Due
     const cashPaid = Number(cash.value).toFixed(2); // 1.87
-    let calculateChangeDue = (cashPaid - price).toFixed(2);
+    let calculateChangeDueTotal = Number((cashPaid - price).toFixed(2));
 
     inputChecker(cashPaid, price);
-    cashClass.calculate(calculateChangeDue);
-    displayChangeDue.innerHTML += `<p class="clicked">Cash Paid: ${cashPaid} - Price: ${price} = Calculate Change Due: ${calculateChangeDue}</p>`;
+    cashClass.calculateChangeDue(calculateChangeDueTotal);
 
 })
