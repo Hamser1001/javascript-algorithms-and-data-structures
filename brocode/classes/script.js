52685c61a3ea9900be16d3c18920af49067b74ad
Hamser1001 <upscale.mind50@gmail.com>
2025-12-23 14:06:58 +0100

    classes with brocode preview

--- brocode/classes/script.js
+++ brocode/classes/script.js
@@ -7,10 +7,19 @@ class Products {
     displayProducts() {
         console.log(`Products: ${this.name}, Price: $${this.price}`);
     }
+
+    calculateTotal() {
+        return this.price + (this.price * salesTax);
+    }
 }
+
+const salesTax = 0.05;
 
 const product1 = new Products("Shirt", 19.99);
 product1.displayProducts();
 
 const product2 = new Products("Pants", 29.99);
-product2.displayProducts();
+// product2.displayProducts();
+
+const total = product1.calculateTotal(salesTax);
+console.log(`Total price (with tax): $${total.toFixed(2)}`);
