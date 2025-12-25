197f63340236722b2eaa854398632a38582fd3c2
Hamser1001 <upscale.mind50@gmail.com>
2025-12-25 15:27:35 +0100

    update

--- brocode/constructor/script.js
+++ brocode/constructor/script.js
@@ -12,4 +12,4 @@ const car2 = new Car("Ford", "Mustang", 2024, "blue");
 console.log(car1.make, car1.model, car1.year, car1.color);
 car1.drive();
 console.log(car2.make, car2.model, car2.year, car2.color);
-car2.drive()
+car2.drive();
