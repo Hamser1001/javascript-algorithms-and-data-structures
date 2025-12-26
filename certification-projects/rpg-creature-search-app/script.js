5addd8ec74ebf88f80985c084b486e54e6d2bcf6
Hamser1001 <upscale.mind50@gmail.com>
2025-12-26 13:12:47 +0100

    update

---
+++ certification-projects/rpg-creature-search-app/script.js
@@ -0,0 +1,18 @@
+const creaturesData = "https://rpg-creature-api.freecodecamp.rocks/api/creatures";
+const creatureData = "https://rpg-creature-api.freecodecamp.rocks/api/creature/{name-or-id}";
+
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const creatureName = document.getElementById("creature-name");
+const creatureId = document.getElementById("creature-id");
+
+const types = document.getElementById("types");
+
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
+
